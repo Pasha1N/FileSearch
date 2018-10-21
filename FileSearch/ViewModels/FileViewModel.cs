2069a34edf8bc65c6ec307590fd1cfd161e220e6
Паша <@DESKTOP-3C2NDOK>
2018-10-21 20:02:39 +0300

    Methods are made: Search(); StopThread(); PauseThread(); GetDirectories(); GetItemsSearching();

---
+++ FileSearch/ViewModels/FileViewModel.cs
@@ -0,0 +1,18 @@
+﻿using System.IO;
+
+namespace FileSearch.ViewModels
+{
+    public class FileViewModel
+    {
+        private string name;
+        private string path;
+
+        public FileViewModel(FileInfo fileInfo)
+        {
+            name = fileInfo.Name;
+            path = fileInfo.FullName;
+        }
+        public string Name => name;
+        public string Path => path;
+    }
+}
--- FileSearch/ViewModels/MainWindowViewModel.cs
+++ FileSearch/ViewModels/MainWindowViewModel.cs
@@ -1,50 +1,120 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
 using System.Windows;
+using System.Windows.Input;
 
 namespace FileSearch.ViewModels
 {
-   public class MainWindowViewModel
+    public class MainWindowViewModel : FileSearch.EventINotifyPropertyChanged
     {
-       private ICollection<string> colaction = new List<string>();
 
-        public  ICollection<string> Colaction => colaction;
+
+        private ICollection<string> discs = new List<string>();
+        private FileSearch.Command.Command commandTheSearch;
+        private FileSearch.Command.Command commandStopThread;
+        private FileSearch.Command.Command commandPauseThread;
+        private ICollection<FileViewModel> files = new ObservableCollection<FileViewModel>();
+        private string mainDirectory;
+        private string selectedItem;
+        private Thread thread = null;
 
         public MainWindowViewModel()
         {
-             Thread thread = new Thread(GetItemsSearching) { IsBackground = true };
-            thread.Start();
+            DisksLoading();
+
+            commandTheSearch = new FileSearch.Command.DelegateCommand(Search);
+            commandStopThread = new FileSearch.Command.DelegateCommand(StopThread);
+            commandPauseThread = new FileSearch.Command.DelegateCommand(PauseThread);
         }
+
+        public ICommand CommandTheSearch => commandTheSearch;
+        public IEnumerable<string> Discs => discs;
+        public string SelectedItem
+        {
+            get => selectedItem;
+            set
+            {
+                selectedItem = value;
+                mainDirectory = selectedItem;
+                OnPropertyChanged(new System.ComponentModel.PropertyChangedEventArgs(nameof(SelectedItem)));
+            }
+        }
+        public IEnumerable<FileViewModel> Files => files;
+
 
         private void Search()
         {
-
-
+           thread = new Thread(GetItemsSearching) { IsBackground = true };
+            thread.Start();
         }
 
+        private void StopThread()
+        {
+            thread.Abort();
+        }
 
+        private void PauseThread()
+        {
+            if (thread.IsAlive)
+            {
+                thread.Suspend();
+            }
+            else
+            {
+                thread.Resume();
+            }
+        }
 
         public void GetItemsSearching()
         {
+            GetDirectories(selectedItem);
+        }
+
+        public void GetDirectories(string path)
+        {
             Application.Current.Dispatcher.Invoke(() =>
             {
-                colaction.Add("fdgd");
-                colaction.Add("fgfh");
-                colaction.Add("67i7j");
-                colaction.Add("vbnc");
-                colaction.Add("zxgvdf");
-                colaction.Add("fdhbfdgh");
-                colaction.Add("gfhndfgt");
+                if (path != null)
+                {
+                    try
+                    {
+                        DirectoryInfo directoryInfo = new DirectoryInfo(path);
+                        DirectoryInfo[] directoryInfos = directoryInfo.GetDirectories();
+
+                        foreach (DirectoryInfo directory in directoryInfos)
+                        {
+                            FileInfo[] currentDirectoryFiles = directory.GetFiles();
+
+                            foreach (FileInfo item in currentDirectoryFiles)
+                            {
+                                files.Add(new FileViewModel(item));
+                            }
+
+                            GetDirectories(directory.FullName);
+                        }
+                    }
+                    catch (UnauthorizedAccessException)
+                    {
+                    }
+                }
             });
         }
 
+        public void DisksLoading()//+
+        {
+            DriveInfo[] driveInfos = DriveInfo.GetDrives();
 
-
-
+            foreach (DriveInfo disc in driveInfos)
+            {
+                discs.Add(disc.Name);
+            }
+            selectedItem = driveInfos[0].Name;
+        }
     }
 }
